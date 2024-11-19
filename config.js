33a7db8c32d11274f3e3dc108d2f51e23ac0a8e5
ejespinoza5 <ejespinoza5@espe.edu.ec>
2024-11-19 00:36:30 -0500

    four commit

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
+// module.exports = {
+//     puerto: 3001,
+//     DB_HOST: process.env.DB_HOST || 'localhost',
+//     DB_USER: process.env.DB_USER || 'root',
+//     DB_PASSWORD: process.env.DB_PASSWORD || '',
+//     DB_NAME: process.env.DB_NAME || 'kanva',
+//     DB_PORT: process.env.DB_PORT || 3306
+// };
+
+
 module.exports = {
     puerto: 3001,
-    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_HOST: process.env.DB_HOST || 'mysql.railway.internal',
     DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || '',
-    DB_NAME: process.env.DB_NAME || 'kanva',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'bEVPOgTOlOOpXgfAkKvZUasqpPZgFTsA',
+    DB_NAME: process.env.DB_NAME || 'railway',
     DB_PORT: process.env.DB_PORT || 3306
 };
