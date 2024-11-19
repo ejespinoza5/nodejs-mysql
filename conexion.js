72b4f014e2810cddc69069f0453133b7c32b28e7
ejespinoza5 <ejespinoza5@espe.edu.ec>
2024-11-19 00:23:42 -0500

    thirst commit

--- conexion.js
+++ conexion.js
@@ -1,16 +1,22 @@
-const {DB_HOST
-  , DB_USER
-  , DB_PASSWORD
-  , DB_NAME
-  , DB_PORT
-} = require('./config.js');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config.js');
 
- var mysql = require('mysql2');
+var mysql = require('mysql2');
 var pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_NAME
+  database: DB_NAME,
+  port: DB_PORT
+});
+
+// Log the connection parameters for debugging
+console.log('Connecting to database with the following parameters:');
+console.log({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
 });
 
 var getConnection = function (cb) {
